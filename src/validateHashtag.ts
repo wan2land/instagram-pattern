5449169902c87a535b4704d62054c13313702df7
Changwan Jun <wan2land@gmail.com>
2022-01-19 18:58:02 +0900

    add tsconfig

--- src/validateHashtag.ts
+++ src/validateHashtag.ts
@@ -1,6 +1,6 @@
 import { RE_HASHTAG_PATTERN, RE_HASHTAG_PATTERN_IGNORE_PREFIX } from './RE_HASHTAG'
 
-export function validateHashtag(hashtag, options: { ignorePrefix?: boolean } = {}) {
+export function validateHashtag(hashtag: string, options: { ignorePrefix?: boolean } = {}) {
   if (options.ignorePrefix) {
     return RE_HASHTAG_PATTERN_IGNORE_PREFIX.test(hashtag)
   }
